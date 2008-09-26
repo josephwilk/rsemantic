9ea71e6bc739df4d7931a58730123d4ceb087ff2
Joseph Wilk <josephwilk@joesniff.co.uk>
2008-09-26 15:16:37 +0100

    Reduce build_vector_* functions to single build_vector.

--- lib/semantic/vector_space/builder.rb
+++ lib/semantic/vector_space/builder.rb
@@ -13,13 +13,13 @@ module Semantic
 
       def build_document_matrix(documents)
         @vector_keyword_index = build_vector_keyword_index(documents)
-        document_vectors = documents.enum_for(:each_with_index).map{|document,document_id| build_vector_from_document(document, document_id)}
+        document_vectors = documents.enum_for(:each_with_index).map{|document,document_id| build_vector(document, document_id)}
         document_matrix = Linalg::DMatrix.join_rows(document_vectors)
       end
 
       #Convert query string into a term vector
       def build_query_vector(term_list)
-        build_vector_from_string(term_list.join(" "))
+        build_vector(term_list.join(" "))
       end
 
       private
@@ -43,19 +43,15 @@ module Semantic
         vector_index  #(keyword:position)
       end
 
-      def build_vector_from_document(document_string, document_id)
-        word_list = @parsed_document_cache[document_id] || @parser.tokenise_and_filter(document_string)
-        build_vector(word_list)
-      end
-
-      def build_vector_from_string(word_string)
-        word_list = @parser.tokenise_and_filter(word_string)
-        build_vector(word_list)
-      end
-
       #Create the keyword associated to the position of the elements within the document vectors
       #@pre: unique(@vector_keyword_index)
-      def build_vector(word_list)
+      def build_vector(word_string, document_id=nil)
+        if document_id.nil?
+          word_list = @parser.tokenise_and_filter(word_string)
+        else
+          word_list = @parsed_document_cache[document_id] || @parser.tokenise_and_filter(document_string)
+        end
+        
         vector = Linalg::DMatrix.new(1, @vector_keyword_index.length)
         word_list.each { |word| vector[0, @vector_keyword_index[word]] += 1 if @vector_keyword_index.has_key?(word)  }
         vector
