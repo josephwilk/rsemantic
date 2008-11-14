9b9fd00894747970ca094b208bd8c2c9f2469873
Joseph Wilk <josephwilk@joesniff.co.uk>
2008-11-14 20:58:35 +0000

    Refactoring to make algorithm better fit mathmatic formula

--- lib/semantic/transform/lsa_transform.rb
+++ lib/semantic/transform/lsa_transform.rb
@@ -4,30 +4,34 @@ module Semantic
 
       class << self
 
-        # Calculate SVD of objects matrix: U . SIGMA . VT = MATRIX
-        # Reduce the dimension of sigma by specified factor producing sigma'.
-        # Then dot product the matrices:  U . SIGMA' . VT = MATRIX'
-        def transform(matrix, dimensions=1)
+        def transform(matrix, number_of_dimensions_to_reduce = 1)
           columns = matrix.num_columns
 
           if dimensions <= columns: #Its a valid reduction
 
             u, sigma, vt = matrix.singular_value_decomposition
 
-            #Dimension reduction, build SIGMA'
-            for index in ((columns-dimensions)...columns)
-              sigma[index,index]=0
-            end
+            sigma_prime = reduce_dimensions(number_of_dimensions_to_reduce, sigma)
 
             #Reconstruct MATRIX' and Save transform
-            matrix = u * sigma * vt
+            matrix_prime = u * sigma_prime * vt
 
           else
             raise Exception, "dimension reduction cannot be greater than %s" % rows
           end
           
+          matrix_prime
+        end
+        
+        private
+        def reduce_dimensions(number_of_dimensions_to_reduce, matrix)
+          columns = matrix.num_columns
+          for index in ((columns-number_of_dimensions_to_reduce)...columns)
+            matrix[index,index] = 0
+          end
           matrix
         end
+        
       end
     end
   end
