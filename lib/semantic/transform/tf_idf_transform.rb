6cfb2bc11ba49eef37406bf5ed5f160d0b71c2bf
Dominik Honnef <dominikh@fork-bomb.org>
2011-04-28 01:32:27 +0800

    rewrote the tf_idf transformer

--- lib/semantic/transform/tf_idf_transform.rb
+++ lib/semantic/transform/tf_idf_transform.rb
@@ -3,22 +3,25 @@ module Semantic
     class TFIDF
 
       @@number_of_documents_with_term = []
-
-      def self.transform(matrix)
+      def self.transform!(matrix)
         number_of_documents = matrix.size2
         @@number_of_documents_with_term = []
 
-        matrix.to_a.transpose.each_with_index do |document, column_index|
-          document_term_total = document.inject(0.0) {|word_sum, word_count| word_sum + word_count.to_f }
+        matrix.transpose.enum_for(:each_row).with_index do |document, column_index|
+          document_term_total = document.sum
 
-          document.to_a.each_with_index do |term_weight, row_index|
-            unless term_weight.to_f == 0.0
-              matrix[row_index, column_index] = (term_weight / document_term_total) *
-              Math.log((number_of_documents / number_of_documents_with_term(row_index, matrix).to_f).abs)
+
+          document.enum_for(:each).with_index do |term_weight, row_index|
+            unless term_weight == 0.0
+              # inverse document frequency
+              idf = GSL::Sf.log((number_of_documents / number_of_documents_with_term(row_index, matrix).to_f).abs)
+              # term frequency
+              tf = (term_weight / document_term_total)
+
+              matrix[row_index, column_index] = tf * idf
             end
           end
         end
-        matrix
       end
 
       def self.number_of_documents_with_term(row_index, matrix)
