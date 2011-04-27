15684131ca7e4a6d5ba3c8da2ae8ebe6a5442443
Dominik Honnef <dominikh@fork-bomb.org>
2011-04-28 01:33:43 +0800

    rewrite lsa transformation

--- lib/semantic/transform/lsa_transform.rb
+++ lib/semantic/transform/lsa_transform.rb
@@ -4,39 +4,28 @@ module Semantic
 
       class << self
 
-        def transform(matrix, number_of_dimensions_to_reduce = 1)
+        def transform!(matrix, rank = 3)
+          # TODO find a smart way to determine a sensible rank
           columns = matrix.size2
 
-          if number_of_dimensions_to_reduce <= columns #Its a valid reduction
-            # u, sigma, vt = matrix.singular_value_decomposition
-            u, vt, sigma = matrix.SV_decomp_mod
-            sigma = GSL::Matrix.diagonal(sigma)
-            sigma_prime = reduce_dimensions(number_of_dimensions_to_reduce, sigma)
+          u, v, sigma = matrix.SV_decomp_mod
+          reduce_dimensions!(sigma, rank)
+          sigma = GSL::Matrix.diagonal(sigma)
 
-            matrix_prime = u * sigma_prime * vt
-          else
-            raise Exception, "dimension reduction cannot be greater than %s" % columns
-          end
-
-          matrix_prime
+          GSL::Matrix.swap(matrix, u * sigma * v.transpose)
         end
 
         private
-        def reduce_dimensions(number_of_dimensions_to_reduce, matrix)
-          for diagonal_index in dimensions_to_be_reduced(matrix, number_of_dimensions_to_reduce)
-            matrix[diagonal_index, diagonal_index] = 0
+        def reduce_dimensions!(vector, rank)
+          # the vector is already sorted (biggest to smallest), so we
+          # only have to zero the elements we do not want
+          if rank > vector.size
+            rank = vector.size
           end
-          matrix
-        end
 
-        def dimensions_to_be_reduced(matrix, number_of_dimensions_to_reduce)
-          (diagonal_matrix_length(matrix) - number_of_dimensions_to_reduce)...diagonal_matrix_length(matrix)
+          num_to_zero_out = vector.size - rank
+          vector[rank, num_to_zero_out] = 0
         end
-
-        def diagonal_matrix_length(matrix)
-          matrix.size2 < matrix.size1 ? matrix.size2 : matrix.size1
-        end
-
       end
     end
   end
