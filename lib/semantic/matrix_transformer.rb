04a6ddd42122dafbd948973cbf3e74bf1f4d3239
Dominik Honnef <dominikh@fork-bomb.org>
2011-04-28 01:32:49 +0800

    transform matrices in place

--- lib/semantic/matrix_transformer.rb
+++ lib/semantic/matrix_transformer.rb
@@ -11,7 +11,7 @@ module Semantic
         begin
           transform_class = Semantic::Transform.const_get(transform)
           Semantic.logger.info("Applying #{transform} transform")
-          vector_space_model.matrix = transform_class.send(:transform, vector_space_model.matrix) if transform_class.respond_to?(:transform)
+          transform_class.transform!(vector_space_model.matrix)
           Semantic.logger.info(vector_space_model)
         rescue => e
           Semantic.logger.error("Error: Cannot perform transform: #{transform}")
