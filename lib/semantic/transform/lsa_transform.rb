edc55a7a5c41258219c26400f716fbd67bae0213
Dominik Honnef <dominikh@fork-bomb.org>
2011-04-28 01:20:17 +0200

    try to guess the best rank for LSA

--- lib/semantic/transform/lsa_transform.rb
+++ lib/semantic/transform/lsa_transform.rb
@@ -4,8 +4,8 @@ module Semantic
 
       class << self
 
-        def transform!(matrix, rank = 3)
-          # TODO find a smart way to determine a sensible rank
+        def transform!(matrix, rank = nil)
+          # TODO configurable rank
           columns = matrix.size2
 
           u, v, sigma = matrix.SV_decomp_mod
@@ -19,9 +19,37 @@ module Semantic
         def reduce_dimensions!(vector, rank)
           # the vector is already sorted (biggest to smallest), so we
           # only have to zero the elements we do not want
-          if rank > vector.size
+
+
+          if rank.nil?
+            # if no specific rank is given, try to determine one
+
+            if vector.size <= 15
+              # for less than 15 documents, n-1 is usually the best we
+              # can do. LSA generally works better with bigger data
+              # sets.
+              rank = vector.size - 1
+            elsif vector.size <= 1000
+              # ~500 is a value to work well for really big data sets,
+              # but for less than that, it probably is too big, so we
+              # go for n/3 in this case.
+              rank = vector.size / 3
+            else
+              # if we have more than 1000 documents, using the magical
+              # number 500 (which can be found in various documents)
+              # seems to be the best guess for now.
+              rank = 500
+            end
+          elsif rank <= 0
+            # for negative ranks, keep that many dimensions
+            rank = vector.size + rank
+          elsif rank > vector.size
+            # if the rank is > the vector size, limit it to that
+
             rank = vector.size
           end
+
+
 
           num_to_zero_out = vector.size - rank
           vector[rank, num_to_zero_out] = 0
