01dcf0190b339b0ddbcda87ceb5072c4c7580489
Dominik Honnef <dominikh@fork-bomb.org>
2011-04-28 17:04:05 +0200

    add classes for a new, nicer API

---
+++ lib/semantic/search_result.rb
@@ -0,0 +1,16 @@
+module Semantic
+  class SearchResult
+    include Comparable
+
+    attr_reader :document
+    attr_reader :score
+    def initialize(document, score)
+      @document = document
+      @score    = score
+    end
+
+    def <=>(other)
+      @score <=> other.score
+    end
+  end
+end
