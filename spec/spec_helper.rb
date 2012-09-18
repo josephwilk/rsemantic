6daf4f96f1ca4e5f2f4f2f1a30cabb7ec02f051c
josephwilk <joe@josephwilk.net>
2012-09-17 22:40:57 -0600

    turn off limited_red for now

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'rspec'
 
 require 'limited_red'
-require 'limited_red/plugins/rspec'
+#require 'limited_red/plugins/rspec'
 
 $:.unshift(File.join(File.dirname(__FILE__), %w[.. lib]))
 
