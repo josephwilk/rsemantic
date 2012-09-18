5265b6e7ce37774741fabcc2422a3f4e4ffaadd0
josephwilk <joe@josephwilk.net>
2012-09-17 21:29:10 -0600

    Adding spec integration

---
+++ spec_integration/semantic_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe "rsemantic" do
+  it "should not die" do
+    documents = ["The cat in the hat disabled", 
+                 "A cat is a fine pet ponies.", 
+                 "Do and cats make good pets.",
+                 "I haven't got a hat."]
+
+    #Log to stdout how the matrix gets built and transformed
+    search = Semantic::Search.new(documents, :verbose => true)
+
+    #We can pass different transforms to be performed. 
+    #Currently only :LSA and :TFIDF. 
+    #The order of transforms reflects the order they will be performed on the matrix
+    # search = Semantic::Search.new(documents, :transforms => [:LSA])
+
+    #Defaults to performing :TFIDF and then :LSA
+    # search = Semantic::Search.new(documents)
+
+    #Find documents that are related to documents[0] with a ranking for how related they are.
+    puts search.related(0)
+
+    #Search documents for the word cat. 
+    #Returns a ranking for how relevant the matches where for each document.
+    puts search.search(["cat"])
+  end
+end
---
+++ spec_integration/spec_helper.rb
@@ -0,0 +1,3 @@
+$:.unshift(File.join(File.dirname(__FILE__), %w[.. lib]))
+
+require 'semantic'
