f94ede74ddfbaf2ecc53c753f312a67396a54da6
josephwilk <joe@josephwilk.net>
2013-01-08 11:59:39 +0100

    formatting

--- lib/semantic/parser.rb
+++ lib/semantic/parser.rb
@@ -8,7 +8,7 @@ module Semantic
       # TODO: nicer way to reference stop file location?
       @filter_stop_words = options[:filter_stop_words]
       @stem_words        = options[:stem_words]
-      locale            = options[:locale] || 'en'
+      locale             = options[:locale] || 'en'
 
       if @filter_stop_words
         File.open("#{File.dirname(__FILE__)}/../../resources/#{locale}.stop", 'r') do |file|
